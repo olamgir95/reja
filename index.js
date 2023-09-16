239e81b7045c76415e60358010565421f1b1ae4b
Olamgir <ollomurodovolamgir233@gmail.com>
2023-09-16 16:53:17 +0900

    practised video lesson

---
+++ index.js
@@ -0,0 +1,29 @@
+const moment = require("moment");
+const date = moment().format("YYYY-MM-DD");
+console.log("Date:", date);
+
+setInterval(() => {
+  const time = moment().format("HH:MM");
+
+  console.log("Time:", time);
+}, 5000);
+
+// const inquirer = require('inquirer');
+
+// inquirer
+//   .prompt([{ type: "input", name: "raqam", message: "raqamni kiriting" }])
+//   .then((answer) => {
+//     console.log("men kiritgan raqam qiymati:", answer.raqam);
+//   })
+//   .catch((err) => console.log(err));
+
+const validator = require("validator");
+const test = validator.isEmail("shskh@kj.com");
+const test2 = validator.isInt("98798");
+
+console.log(test);
+console.log(test2);
+
+const { v4: uuidv4 } = require('uuid');
+const random = uuidv4();
+console.log(random);
--- tran.js
+++ tran.js
@@ -4,4 +4,4 @@ function number(a) {
   let b = a.split("").filter((vl) => vl === Number(vl).toString())
   return console.log(b.length);
 }
-number("jecdjewcgwjkq   dqdwqw7347478268hwk9");
+number("dqdwqw7347478268hwk9");
