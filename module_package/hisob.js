406e7cf9e48f83fd80cce7596a79335eec902a9d
Olamgir <ollomurodovolamgir233@gmail.com>
2023-09-18 21:50:15 +0900

    tasks finished

---
+++ module_package/hisob.js
@@ -0,0 +1,17 @@
+
+
+const Account=require("./account")
+Account.tellMeTime()
+Account.tellMeAboutClass()
+
+
+const myAccount= new Account("Jonnhy", 500000, 8748678497097)
+myAccount.giveMeDetails()
+
+myAccount.makeDeposit(100000)
+
+
+// ferrari 400000usd
+
+myAccount.withdrawMoney(400000)
+myAccount.makeDeposit(2000000)
